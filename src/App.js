c067e2c23a6e1491c4333a6d98c451f084932fad
Nathan Cha <nathancha777@gmail.com>
2020-07-15 10:51:37 -0400

    weather app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import WeekContainer from './Compontents/WeekContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <WeekContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/apiKey.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "ApiKey": "ad2955322c8df53513c3318f6db1419e",
+    
+}
